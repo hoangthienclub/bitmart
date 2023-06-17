24f5a111903cd200a4aca0ca442767a63dbd8b92
Woverlin <Woverlin.96@gmail.com>
2023-06-17 14:48:49 +0700

    update UI

--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -33,9 +33,7 @@ const SearchInput = ({
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg">
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+            
               d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
           </svg>
         </div>
@@ -51,12 +49,18 @@ const SearchInput = ({
       </div>
       {isShowSearchResult && <div className="absolute z-10 mt-2 w-[400px] max-h-[400px] overflow-auto bg-white rounded-lg shadow dark:bg-gray-700">
         {dataSearch?.map((it: ISymbol, index: number) => {
-          return (<div onClick={() => {
-            setIsShowSearchResult(false)
-            onSelectSymbol(it)
-            }}>
-            <div key={index} className="cursor-pointer block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{it?.symbol}</div>
-          </div>)
+          return (
+            <div
+              key={index}
+              onClick={() => {
+                setIsShowSearchResult(false);
+                onSelectSymbol(it);
+              }}>
+              <div className="cursor-pointer block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                {it?.symbol}
+              </div>
+            </div>
+          );
         })}
       </div>
       }
